5c6f85d07915cf85bf44b65273182b30d219b7f3
wsteffe <walter.steffe.it@gmail.com>
2023-09-22 09:00:15 +0200

    layer cleaned at the end of mapLayers(). Added lexicographic layer mapping

--- klayout_dot_config/python/layoutDD/mapLayers.py
+++ klayout_dot_config/python/layoutDD/mapLayers.py
@@ -1,46 +1,58 @@
 
-mapping = { 
-    "L0D0_0"     : [ 0, 0],
-    "STICH-VIAS" : [10, 0],
-    "BOTTOM"     : [11, 0],
-    "TOP"        : [12, 0],
-    "RESISTANCE" : [13, 0],
-}
+#mapping = { 
+#    "L0D0_0"     : [ 0, 0],
+#    "STICH-VIAS" : [10, 0],
+#    "BOTTOM"     : [11, 0],
+#    "TOP"        : [12, 0],
+#    "RESISTANCE" : [13, 0],
+#}
 
 import pya
 
-def mapLayers():
-    global mapping
-    layoutView = pya.Application.instance().main_window().current_view()
-    cellView   = layoutView.active_cellview()
-    layout     = cellView.layout()
+def makeLayerMap(layoutView,cellView):
     itr = layoutView.begin_layers()
+    layerNames=[]
     while not itr.at_end():
         lyp = itr.current()
-
         if lyp.cellview() == cellView.index():
-            lid = lyp.layer_index()
-            lnm = lyp.source_name
-
-            if lnm in mapping:
-                lno, ldt = mapping[ lnm ]
-                layout.set_info(lid, pya.LayerInfo(lno, ldt, lnm))
+            if lyp.source_layer < 0:
+                layerNames.append(lyp.source_name)
         itr.next()
-    layoutView.add_missing_layers()
+    layerNames.sort()
+    layerMap={}
+    ln=0
+    dt=0
+    for name in layerNames:
+        ln=ln+1
+        layerMap[name]=[ln, dt]
+    return layerMap
 
-def cleanLayers():
-    layoutView = pya.Application.instance().main_window().current_view()
-    cellView   = layoutView.active_cellview()
+def cleanLayers(layoutView,cellView):
     itr = layoutView.begin_layers()
     while not itr.at_end():
         lyp = itr.current()
         if lyp.cellview() == cellView.index():
-
             if (lyp.source_layer < 0):
                 layoutView.delete_layer(itr)
             else:
                 itr.next()
-    layoutView.add_missing_layers() 
 
-#mapLayers()
-#cleanLayers()
+def mapLayers():
+    layoutView = pya.Application.instance().main_window().current_view()
+    cellView   = layoutView.active_cellview()
+    layout     = cellView.layout()
+    layerMap=makeLayerMap(layoutView,cellView)
+    itr = layoutView.begin_layers()
+    while not itr.at_end():
+        lyp = itr.current()
+        if lyp.cellview() == cellView.index():
+            lid = lyp.layer_index()
+            lnm = lyp.source_name
+            if lnm in layerMap:
+                lno, ldt = layerMap[ lnm ]
+                layout.set_info(lid, pya.LayerInfo(lno, ldt, lnm))
+        itr.next()
+    cleanLayers(layoutView,cellView)
+    layoutView.add_missing_layers()
+
+
